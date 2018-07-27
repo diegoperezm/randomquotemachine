0defb92706283c6953fe1f398c7cff7d4aaf44bb
diegoperezm <diegoperezcorreo@gmail.com>
2018-07-27 09:59:40 -0400

    Replace quoteGenerator.currentNumber with a local variable.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,215 @@
-
 var variable = {
  divText: document.getElementById("text"),
  paragraph : document.getElementById("paragraph"),
  buttonGetQuote: document.getElementById("buttonGetQuote"),
  quotes: [
-  "Carly Rae Jepsen (Curiosity)",
-  "I threw a wish in the well",
-  "Don't ask me I'll never tell",
-  "I looked at you as it fell And now you're in my way."
+"I threw a wish in the well",
+"Don't ask me I'll never tell",
+"I looked to you as it fell",
+"And now you're in my way",
+"I'd trade my soul for a wish",
+"Pennies and dimes for a kiss",
+"I wasn't looking for this",
+"But now you're in my way",
+"Your stare was holding",
+"Ripped jeans, skin was showin'",
+"Hot night, wind was blowin'",
+"Where you think you're going baby?",
+"Hey, I just met you and this is crazy",
+"But here's my number, so call me maybe",
+"It's hard to look right at you baby",
+"But here's my number, so call me maybe",
+"Hey I just met you and this is crazy",
+"But here's my number, so call me maybe",
+"And all the other boys try to chase me",
+"But here's my number, so call me maybe",
+"You took your time with the call",
+"I took no time with the fall",
+"You gave me nothing at all",
+"But still you're in my way",
+"I beg and borrow and steal",
+"At first sight and it's real",
+"I didn't know I would feel it",
+"But it's in my way",
+"Your stare was holding",
+"Ripped jeans, skin was showin'",
+"Hot night, wind was blowin'",
+"Where you think you're going baby?",
+"Hey, I just met you and this is crazy",
+"But here's my number, so call me maybe",
+"It's hard to look right at you baby",
+"But here's my number, so call me maybe",
+"Hey I just met you and this is crazy",
+"But here's my number, so call me maybe",
+"And all the other boys try to chase me",
+"But here's my number, so call me maybe",
+"Before you came into my life",
+"I missed you so bad",
+"I missed you so bad",
+"I missed you so so bad",
+"Before you came into my life",
+"I missed you so bad",
+"And you should know that",
+"I missed you so so bad",
+"It's hard to look right at you baby",
+"But here's my number, so call me maybe",
+"Hey, I just met you and this is crazy",
+"But here's my number, so call me maybe",
+"And all the other boys try to chase me",
+"But here's my number, so call me maybe",
+"Before you came into my life",
+"I missed you so bad",
+"I missed you so bad",
+"I missed you so so bad",
+"Before you came into my life",
+"I missed you so bad",
+"And you should know that",
+"So call me maybe",
+"Sing to the night without sight but with madness",
+"I can't keep up, I'm a wreck but I want it",
+"Sing to the night without sight but with madness",
+"I can't keep up, I'm a wreck but I want it",
+"Kids in the street drinking wine, on the sidewalk",
+"Saving the plans that we made, 'til it's night time",
+"Give me your glass, it's your last, you're too wasted",
+"Get me one too, 'cause I'm due any tasting",
+"Kids in the street drinking wine, on the sidewalk",
+"Saving the plans that we made, 'til it's night time",
+"Give me your glass, it's your last, you're too wasted",
+"Get me one too, 'cause I'm due any tasting",
+"I've lost my way, it's hard to find it through",
+"I see the night but I'm lost outside of you",
+"I've lost my way, it's hard to find it through",
+"I see the night but I'm lost outside of you",
+"I've lost my way, it's hard to find it through",
+"I see the night, but I'm lost outside of you",
+"I've lost my way, it's hard to find it through",
+"I see the night, but I'm lost outside of you",
+"I've lost my way, it's hard to find it through",
+"I see the night, but I'm lost outside of you",
+"I've lost my way, it's hard to find it through",
+"I see the night, but I'm lost outside of you",
+"Been there, done that, messed around",
+"I'm having fun, don't put me down",
+"I'll never let you sweep me off my feet",
+"I won't let you in again",
+"The messages I've tried to send",
+"My information's just not going in",
+"Burning bridges shore to shore",
+"I'll break away from something more",
+"I'm not turned on to love until it's cheap",
+"Been there, done that, messed around",
+"I'm having fun, don't put me down",
+"I'll never let you sweep me off my feet",
+"This time, baby, I'll be",
+"Bulletproof",
+"This time, baby, I'll be",
+"Bulletproof",
+"I won't let you turn around",
+"And tell me now I'm much too proud",
+"To walk away from something when it's dead",
+"Do, do, do your dirty words",
+"Come out to play when you are heard",
+"There's certain things that should be left unsaid",
+"Tick, tick, tick, tick on the watch",
+"And life's too short for me to stop",
+"Oh, baby, your time is running out",
+"I won't let you turn around ",
+"And tell me now I'm much too proud",
+"All you do is fill me up with doubt",
+"This time, baby, I'll be",
+"Bulletproof",
+"This time, baby, I'll be",
+"Bulletproof",
+"This time, baby, I'll be",
+"Bulletproof",
+"This time, baby, I'll be",
+"Bulletproof",
+"This time baby",
+"I'll be",
+"Bulletproof",
+"This time baby",
+"I'll be",
+"Bulletproof",
+"This time, baby, I'll be",
+"Bulletproof",
+"This time, baby, I'll be",
+"Bulletproof",
+"This time, baby, I'll be",
+"Bulletproof",
+"This time, baby, I'll be",
+"Bulletproof",
+"Late night, waiting by the phone",
+"Tonight waiting for an answer",
+"Heartbeat drumming double time",
+"I need one more chance to be near you",
+"Still hanging on",
+"(For what)",
+"Can't operate",
+"(Fired up)",
+"No rest till I",
+"(Get through)",
+"'Cause I'm holding out",
+"(For you)",
+"Am I the only one who's insane?",
+"Hey, you're playing with my delirium",
+"And the longer I wait the harder I'm gonna fall",
+"Stop playing with my delirium",
+"'Cause I'm outta my head and outta my self-control",
+"Still here in this quiet room",
+"Deep in delusion sending me over",
+"Outside watch the world go by",
+"Inside time stands still as I wonder",
+"Still hanging on",
+"(For what?)",
+"Can't operate",
+"(Fired up)",
+"No rest till I",
+"(Get through)",
+"'Cause I'm holding out",
+"(For you)",
+"Am I the only one who's insane",
+"Hey, you're playing with my delirium",
+"And the longer I wait the harder I'm gonna fall",
+"Stop playing with my delirium",
+"'Cause I'm outta my head and outta my self-control",
+"Hey, you're playing with my delirium",
+"And the longer I wait the harder I'm gonna fall",
+"Stop playing with my delirium",
+"'Cause I'm outta my head and outta my self-control",
+"Hey, you're playing with my delirium",
+"And the longer I wait the harder I'm gonna fall",
+"Stop playing with my delirium",
+"Cause I'm outta my head and outta my self-control",
+"When you hide next to me",
+"With your eyes up close to me",
+"I hear footsteps in the dark",
+"When you hijack my heart",
+"Cool breeze down the hall",
+"I can feel you breathe outside my door",
+"Bang, bang, bang on the wall",
+"From dusk 'til dawn",
+"Do, do, do, do",
+"Do, do, do, do",
+"Bang, bang, bang on the wall",
+"From dusk 'til dawn",
+"Do, do, do, do",
+"Do, do, do, do",
+"Bang, bang, bang on the wall",
+"From dusk 'til dawn",
+"When you sense you're not alone",
+"And the darkness starts to moan who's there?",
+"Shadows all around",
+"But you don't make a sound",
+"Voices in my head",
+"Playing like an echo, echo",
+"Bang, bang, bang on the wall",
+"From dusk 'til dawn",
+"Do, do, do, do",
+"Do, do, do, do"
 ]
 };
+
 
 
 /*
@@ -46,10 +246,7 @@ var fsm = {
 
 
 var quoteGenerator =  {
-
   currentQuote : "",
-  currentNumber: 0,
-
 /**
  * @param {Object}     variable - Data and DOM elements   
  * @param {String} currentState   
@@ -100,11 +297,20 @@ function transition ( variable, currentState, action, error) {
 function getQuote(variable, currentState) {
 
   var quotes = variable.quotes;
+
+/*
+ * Trying to imitate an async behavior, no performance issues in my box
+ * Later: make a proper API call 
+ */
   var quote =  Promise.resolve(quotes); 
 
   quote
      .then((elements) => { 
-      setQuote(variable, elements[quoteGenerator.currentNumber % 4]); 
+      var min = 0;
+      var max = variable.quotes.length;
+      var quoteIndex = Math.floor(Math.random() * (max - min) + min);
+
+      setQuote(variable, elements[quoteIndex]); 
       transition(variable, currentState, "sucess");
      })
      .catch( error => {
@@ -127,7 +333,6 @@ function setQuote(variable, quote) {
  }
 
  quoteGenerator.currentQuote = quote;
- quoteGenerator.currentNumber += 1 ;
 }
 
 
@@ -139,7 +344,6 @@ function addQuoteToPage(variable) {
  var paragraph = variable.paragraph;  
  paragraph.textContent = quoteGenerator.currentQuote; 
 }
-
 
 /**
  * @param {Object}          variable - Data and DOM elements 
@@ -154,6 +358,7 @@ function displayError(variable, currentState, action, error) {
 
     addErrorToPage(variable, error);
 }
+
 
 
 /**
@@ -175,5 +380,4 @@ function addErrorToPage(variable, error) {
 
 
 quoteGenerator.start(variable, fsm.currentState);
-
 
